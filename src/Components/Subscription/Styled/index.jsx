382d67c1eddc8ef8e13f3139ba3ecbed581646d5
recafox <recafox@gmail.com>
2021-10-25 12:30:10 +0800

    fix background image in subscription

--- src/Components/Subscription/Styled/index.jsx
+++ src/Components/Subscription/Styled/index.jsx
@@ -4,8 +4,8 @@ export const StyledSubscriptionWrapper = styled.div`
   margin-top: 202px;
   margin-bottom: 117px;
   background-image: url('./assets/bg-footer-squiggle.svg');
-  background-size: 584px 80px;
-  background-position-y: 0;
+  background-size: 1665px 228px;
+  background-position: center top;
   background-repeat: no-repeat;
   height: 228px;
   display: flex;
@@ -13,11 +13,16 @@ export const StyledSubscriptionWrapper = styled.div`
   align-items: center;
   justify-content: center;
 
+  @media(max-width: ${({ theme }) => theme.breakPoints.tablet}) {
+    background-size: 1022px 140px;
+  }
+
   @media(max-width: ${({ theme }) => theme.breakPoints.mobile}) {
     width: 100%;
     margin-left: auto;
     margin-right: auto;
     margin-top: 144px;
+    background-size: 584px 80px;
 
     h4 {
       width: 327px;
